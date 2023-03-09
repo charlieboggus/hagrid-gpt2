8ee70f94a0b88ad2837be5b82bbb6c04325ec7b2
Charlie Boggus <charlie.boggus@gmail.com>
2023-03-09 12:04:03 -0500

    built out tokenizer script

---
+++ src/tokenize_data.py
@@ -0,0 +1,42 @@
+import os
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.decoders import ByteLevel as ByteLevelDecoder
+from tokenizers.normalizers import NFKC, Sequence
+from tokenizers.pre_tokenizers import ByteLevel
+from tokenizers.trainers import BpeTrainer
+from pathlib import Path
+
+class BPE_token(object):
+    def __init__ (self):
+        self.tokenizer = Tokenizer(BPE())
+        self.tokenizer.normalizer = Sequence([NFKC()])
+        self.tokenizer.pre_tokenizer = ByteLevel()
+        self.tokenizer.decoder = ByteLevelDecoder()
+
+    def bpe_train (self, paths):
+        trainer = BpeTrainer(
+            vocab_size=50000, 
+            show_progress=True,  
+            initial_alphabet=ByteLevel.alphabet(),
+            special_tokens=[
+                "<s>",
+                "<pad>",
+                "</s>",
+                "<unk>",
+                "<mask>"
+            ]
+        )
+        self.tokenizer.train(trainer, paths)
+    
+    def save_tokenizer (self, location, prefix=None):
+        if not os.path.exists(location):
+            os.makedirs(location)
+        self.tokenizer.model.save(location, prefix)
+
+# Tokenize our data
+paths = [str(x) for x in Path("./data/working/").glob("**/*.txt")]
+tk = BPE_token()
+tk.bpe_train(paths)
+save_path = 'tokenized'
+tk.save_tokenizer(save_path)
